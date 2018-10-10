10ad49edeca4fb3a8664c30d390a7c92ba9101d0
ouyang liubin <oylbin@ouyangs-Mac-mini.local>
2018-10-10 22:35:15 +0800

    add --max-time to avoid long time running

--- website-speed.sh
+++ website-speed.sh
@@ -1,4 +1,4 @@
 url=$1
 
 # url 里不能有空格、单引号、双引号
-curl -s -w 'time_namelookup{url="'$url'"}\t%{time_namelookup}\ntime_connect{url="'$url'"}\t%{time_connect}\ntime_appconnect{url="'$url'"}\t%{time_appconnect}\ntime_pretransfer{url="'$url'"}\t%{time_pretransfer}\ntime_redirect{url="'$url'"}\t%{time_redirect}\ntime_starttransfer{url="'$url'"}\t%{time_starttransfer}\ntime_total{url="'$url'"}\t%{time_total}\n' -o /dev/null "$url"
+curl --max-time 15 -s -w 'time_namelookup{url="'$url'"}\t%{time_namelookup}\ntime_connect{url="'$url'"}\t%{time_connect}\ntime_appconnect{url="'$url'"}\t%{time_appconnect}\ntime_pretransfer{url="'$url'"}\t%{time_pretransfer}\ntime_redirect{url="'$url'"}\t%{time_redirect}\ntime_starttransfer{url="'$url'"}\t%{time_starttransfer}\ntime_total{url="'$url'"}\t%{time_total}\n' -o /dev/null "$url"
